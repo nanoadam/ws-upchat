6c3e6a1f62d897bc8a7a6181fbde2e133ea7a9b6
Nano Adam <nanoproductions2@gmail.com>
2020-07-29 12:05:44 -0400

    Fixing the Bug

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,13 +2,16 @@ import axios from "axios";
 import ENDPOINT from "../config/ENDPOINT";
 
 export const login = (email, password) => async (dispatch) => {
+  console.log(email, password);
   try {
     const res = await axios.post(ENDPOINT + "/api/auth/login", {
       email: email,
       password: password,
     });
 
-    await console.log(res.data);
+    dispatch();
+
+    await console.log(res);
   } catch (err) {
     console.log(err);
   }
